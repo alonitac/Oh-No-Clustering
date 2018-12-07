47545b56489f49cf1950f3e4bbcda22203bb426f
FranziskaSW <franziska.wehrmann@gmail.com>
2018-12-07 19:24:15 +0200

    completed part c - Three Guassians

--- synthetic_data.py
+++ synthetic_data.py
@@ -24,23 +24,35 @@ if __name__ == '__main__':
     plt_show(x, y, 'Gaussian with center at [1,1] and std=2')
 
     # (c)
-    # to be implemented
+    params = {0: {'mean': [1, -1],
+                  'cov': [[2, 0], [0, 2]]},
+              1: {'mean': [2, -2],
+                  'cov': [[16, 0], [0, 16]]},
+              2: {'mean': [3, -3],
+                  'cov': [[36, 0], [0, 36]]}
+              }
+    mixture_idx = np.random.choice(a=3, size=300, replace=True)
+
+    x_cord, y_cord = [], []
+    for i in mixture_idx:
+        x, y = np.random.multivariate_normal(params[i]['mean'], params[i]['cov'], 1).T
+        x_cord.append(x[0])
+        y_cord.append(y[0])
+    plt_show(x_cord, y_cord, 'Three Gaussians')
 
     # (d)
-    # to be implemented
     x, y = np.random.uniform(-1, 1, N), np.random.uniform(-1, 1, N)
     r1, r2, r3 = 1, 0.75, 0.25
 
     x_cord, y_cord = [], []
     i = 0
-    while (len(x_cord) < 300):
-        if ((x[i]**2 < r1**(2) - y[i]**2) and (x[i]**2 > r2**(2) - y[i]**2)) or (x[i]**2 < r3**(2) - y[i]**2):
+    while len(x_cord) < 300:
+        if ((x[i]**2 < r1**2 - y[i]**2) and (x[i]**2 > r2**2 - y[i]**2)) or (x[i]**2 < r3**2 - y[i]**2):
             x_cord.append(x[i])
             y_cord.append(y[i])
         i += 1
 
-    plt_show(x_cord, y_cord, 'circle')
-
+    plt_show(x_cord, y_cord, 'CURE Circles')
 
     # (e)
     # sample from F
