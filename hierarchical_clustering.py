184322818480590b22eda31496d6d6ce2e4221ed
Alon Itach <alon.itach@mail.huji.ac.il>
2018-12-07 17:30:31 +0200

    finish q4

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 import numpy as np
 import json
 
@@ -7,7 +7,7 @@ import json
 
 if __name__ == '__main__':
 
-    dolars_pelged = []  # each elment is a tuple:  (<id>, <DolarsPelged>)
+    dolars_pelged = []  # each element is a tuple:  (<id>, <DolarsPelged>)
 
     # read & clean the json
     with open('ex1.json') as f:
@@ -18,17 +18,42 @@ if __name__ == '__main__':
     n = len(dolars_pelged)
     points = np.array([d[1] for d in dolars_pelged], ndmin=2)
 
+    # show the points
+    plt.figure()
+    plt.title('The points')
+    plt.scatter(points, [0] * points.shape[1])
+    plt.show()
+
     # perform hierarchical clustering (divisive)
-    # I use centroid methods. see here list of methods: https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.linkage.html#scipy.cluster.hierarchy.linkage
     levels = linkage(points.T, 'centroid')
 
-    # print(levels[0]) - information about level 0 of the tree. each level is a merge between 2 clusters.
-    # the following will be printed:
-    # [cluster1 index , cluster2 index, distance between clusters, number of points in the merged cluster]
-
+    # plot distances as function of level
     plt.plot(range(n-1), levels[:, 2])
-    plt.title('distances throughout the algorithm')
+    plt.title('Centroid distances')
     plt.xlabel('Level')
-    plt.ylabel('Distance of Centroids')
+    plt.ylabel('Distance')
     plt.show()
+
+    # plot how many clusters for certain distance
+    m = levels[-1, 2]  # last merging distance
+
+    plt.title('Number of clusters for m={}'.format(int(m)))
+    plt.xlabel('Index')
+    plt.ylabel('Distance')
+    dendrogram(levels)
+    plt.axhline(y=0.1 * m, color='black', ls='--', label='4 clusters for 0.1m')
+    plt.axhline(y=0.5 * m, color='red', ls='--', label='2 clusters for 0.5m')
+    plt.legend()
+    plt.show()
+
+    # retrieve the clusters
+    k = 5
+    clusters = fcluster(levels, k, criterion='maxclust')
+    plt.figure()
+    plt.title('The clusters for k={}'.format(5))
+    plt.scatter(points, [0]*points.shape[1], c=clusters, cmap='Set1')
+    plt.show()
+
+
+
 
